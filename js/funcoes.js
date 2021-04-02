737f08039c0a8d12a9def03c5084c9d34f95f441
joaopedrobordin <bpj7@hotmail.com>
2021-04-01 22:32:15 -0300

    js.feito

---
+++ js/funcoes.js
@@ -0,0 +1,8 @@
+let filmes = ["soul" , "florest gunp" , "de volta para o futuro 1","pepapy o retorno"]
+console.log(filmes);
+console.log(filmes.length);
+
+let quantidadesDeFilmes = 3;
+while(quantidadesDeFilmes > 0)
+{console.log(filmes[quantidadesDeFilmes]);
+quantidadesDeFilmes --;}
